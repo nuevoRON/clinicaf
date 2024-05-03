29f5212f5d9d4f658e963d086d560b1415b8a11c
nuevoRON <hronaldjosue@gmail.com>
2024-05-03 08:28:04 -0600

    login

--- login.php
+++ login.php
@@ -4,30 +4,8 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<title>Login</title>
+	<?php include "./inc/link.php"; ?>
 
-	<!-- Normalize V8.0.1 -->
-	<link rel="stylesheet" href="./css/normalize.css">
-
-	<!-- Bootstrap V4.3 -->
-	<link rel="stylesheet" href="./css/bootstrap.min.css">
-
-	<!-- Bootstrap Material Design V4.0 -->
-	<link rel="stylesheet" href="./css/bootstrap-material-design.min.css">
-
-	<!-- Font Awesome V5.9.0 -->
-	<link rel="stylesheet" href="./css/all.css">
-
-	<!-- Sweet Alerts V8.13.0 CSS file -->
-	<link rel="stylesheet" href="./css/sweetalert2.min.css">
-
-	<!-- Sweet Alert V8.13.0 JS file -->
-	<script src="./js/sweetalert2.min.js" ></script>
-
-	<!-- jQuery Custom Content Scroller V3.1.5 -->
-	<link rel="stylesheet" href="./css/jquery.mCustomScrollbar.css">
-	
-	<!-- General Styles -->
-	<link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
 
@@ -49,7 +27,7 @@
 					<input type="password" class="form-control" id="UserPassword" name="clave" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" required="" >
 				</div>
 				<button type="submit" class="btn-login text-center">LOGIN</button>
-				<a href="user-list.html"></a>
+				<a href="home.php"></a>
 			</form>
 		</div>
 	</div>
@@ -58,22 +36,6 @@
 	<!--=============================================
 	=            Include JavaScript files           =
 	==============================================-->
-	<!-- jQuery V3.4.1 -->
-	<script src="./js/jquery-3.4.1.min.js" ></script>
-
-	<!-- popper -->
-	<script src="./js/popper.min.js" ></script>
-
-	<!-- Bootstrap V4.3 -->
-	<script src="./js/bootstrap.min.js" ></script>
-
-	<!-- jQuery Custom Content Scroller V3.1.5 -->
-	<script src="./js/jquery.mCustomScrollbar.concat.min.js" ></script>
-
-	<!-- Bootstrap Material Design V4.0 -->
-	<script src="./js/bootstrap-material-design.min.js" ></script>
-	<script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
-
-	<script src="./js/main.js" ></script>
+	<?php include "./in/java.php"; ?>
 </body>
 </html>
