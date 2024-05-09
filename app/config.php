f0f7db009a0fc28d6185885fa33e711fa28650dc
nuevoRON <hronaldjosue@gmail.com>
2024-05-09 09:41:57 -0700

    combio en pdo

--- app/config.php
+++ app/config.php
@@ -1,15 +1,15 @@
 <?php
 
 /* definicion  de variables para la coneccion a la base de datos */
-$servidor = "localhost";
-$usuario = "root";
-$password = "";
+$servidor = "127.0.0.1";
+$usuario = "postgres";
+$password = "ubuntu";
 $bd = "clinica";
 
 //podemos cambiar el mysql de la linea del PDO("mysql: por cualquier otro gestor de BD y ser igual
 
 try{
-    $conexion = new PDO("mysql:host=$servidor;dbname=$bd", $usuario, $password);
+    $conexion = new PDO("pgsql:host=$servidor;dbname=$bd", $usuario, $password);
     $conexion-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "la coneccion a la base ha sido exitosa";
 }
