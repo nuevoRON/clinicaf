9bc0ec8a5b62c48a16e2053ab53c563602c05a2e
nuevoRON <hronaldjosue@gmail.com>
2024-06-14 10:43:28 -0600

    centrado

--- evaluacion-casos.php
+++ evaluacion-casos.php
@@ -178,15 +178,13 @@
                                     </select>
 								</div>
                             </div>
-                            </div>
-                        </div>
-                        
-                        <p class="text-center" style="margin-top: 40px;">
+                            <p class="text-center" style="margin-top: 40px;">
 							<button type="reset" class="btn btn-raised btn-secondary btn-sm"><i class="fas fa-paint-roller"></i> &nbsp; LIMPIAR</button>
 							&nbsp; &nbsp;
 							<button type="submit" class="btn btn-raised btn-info btn-sm"><i class="far fa-save"></i> &nbsp; GUARDAR</button>
 						</p>
-                       
+                            </div>
+                        </div>                       
                     </div>
                 </div>
             </div>
